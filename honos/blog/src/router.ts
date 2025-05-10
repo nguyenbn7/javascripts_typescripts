2e263925ccdf0950a5540f0dd090f43a3b5d5bba
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-05-10 13:19:31 +0700

    add tag route

--- honos/blog/src/router.ts
+++ honos/blog/src/router.ts
@@ -2,7 +2,11 @@ import { Hono } from "hono";
 
 import categories from "./categories/router";
 import posts from "./posts/router";
+import tags from "./tags/router";
 
-const app = new Hono().route("/categories", categories).route("/posts", posts);
+const app = new Hono()
+  .route("/categories", categories)
+  .route("/posts", posts)
+  .route("/tags", tags);
 
 export default app;
